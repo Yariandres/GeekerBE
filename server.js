dcbcc5403a45a5bf68bac6b3fc058f74d85fd34a
Yari Andres <yari.andres@gmail.com>
2020-05-26 13:04:48 +0200

    Connecting to DB

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db.js');
 
 const app = express();
+
+// Connects to DB server
+connectDB();
 
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API running'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
